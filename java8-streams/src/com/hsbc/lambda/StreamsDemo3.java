b5d35cbc3a6eaa164130a03b35edf2ac94fa109d
shan71 <sbtalk@hotmail.com>
2021-09-22 17:17:27 +0530

    maven examples added

--- java8-streams/src/com/hsbc/lambda/StreamsDemo3.java
+++ java8-streams/src/com/hsbc/lambda/StreamsDemo3.java
@@ -17,7 +17,24 @@ public class StreamsDemo3 {
 		empList.add(new Employee(105, "Pretham", 80000));
 		empList.add(new Employee(106, "Raja", 85000));
 		empList.add(new Employee(105, "Pretham", 66000));
+
+		// print details of all employees with salary >65000
+		empList.stream().filter(e -> e.getSalary() > 65000).forEach(System.out::println);
+
+		// update Name
+		empList.stream().filter(e -> e.getSalary() > 65000).map(e -> {
+			e.setName("Hello " + e.getName());
+			return e;
+		}).forEach(System.out::println);
+		;
+		// add the salaries of employees which is less than 65000
+		double salary = empList.stream().filter(e -> e.getSalary() < 65000).mapToDouble(e -> e.getSalary())
+				.reduce((x, y) -> x + y).getAsDouble();
+		System.out.println(salary);
 		
+		// list all those names which contains Sh
+		empList.stream().filter(e -> e.getName().contains("Sh")).forEach(System.out::println);
+		// add their salaries
 	}
 
 }
